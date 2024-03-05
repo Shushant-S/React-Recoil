d57c1e4b61e5d2fad708d1d5196f46acd1d7a221
Shushant-S <shushant0180gmail.com>
2024-03-05 07:44:19 +0530

    added atom-family to the repo

---
+++ 3. atom-family/src/App.jsx
@@ -0,0 +1,25 @@
+import { RecoilRoot, useRecoilValue } from 'recoil'
+import './App.css'
+import { todosAtomFamily } from './atom'
+
+function App() {
+
+  return <RecoilRoot>
+      <Todo id={1}/>
+      <Todo id={2}/>
+    </RecoilRoot>
+}
+
+function Todo({id}){
+  const currentTodo = useRecoilValue(todosAtomFamily(id));
+
+  return (
+    <>
+      {currentTodo.title}
+      {currentTodo.description}
+      <br />
+    </>
+  )
+}
+
+export default App;
